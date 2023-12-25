e08cba0adc2250a9c3c65747bc8b97f86b873a78
Michal Zajac <michal.zajac@vw-fc.com>
2023-12-25 18:09:16 +0100

    updated

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
-﻿using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-using NUnit.Framework;
-using NUnit.Framework.Internal;
+﻿
 
-Main();
 
-static void Main()
-{
-  string[] input = new string[] {"tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"};
-  Console.WriteLine($"Output is: {LongestConsecutives.LongestConsec(input, 2)}");
-}
+string[] input = new string[] {"tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"};
+Console.WriteLine($"Output is: {LongestConsecutives.LongestConsec(input, 2)}");
+
 public class LongestConsecutives 
 {
     
-    public static string LongestConsec(string[] strarr, int k) 
-    {
-        return "";
+    public static string[] LongestConsec(string[] strarr, int k) 
+    { 
+        string[] concatStrings = new string[strarr.Length];
+
+        for (int i = 0; i < strarr.Length-1; i++)
+        {
+          concatStrings[0] = string.Join("", strarr[i] + strarr[i+1]);
+        }
+
+        foreach (var element in concatStrings)
+        {
+          
+        }
+
+        return concatStrings;
     }
 }
