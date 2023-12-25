cffd794bcf1e04623192947d1433b0f8b915ebbb
Michal Zajac <michal.zajac@vw-fc.com>
2023-12-25 16:31:22 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+Main();
+
+static void Main()
+{
+  string[] input = new string[] {"tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"};
+  LongestConsecutives.LongestConsec(input, 2);
+}
+public class LongestConsecutives 
+{
+    
+    public static string LongestConsec(string[] strarr, int k) 
+    {
+        return "";
+    }
+}
