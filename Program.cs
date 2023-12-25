c74ac56567312569da3e143e67e45095925b6d52
Michal Zajac <michal.zajac@vw-fc.com>
2023-12-25 18:27:28 +0100

    solution to https://www.codewars.com/kata/56a5d994ac971f1ac500003e

--- Program.cs
+++ Program.cs
@@ -7,20 +7,35 @@ Console.WriteLine($"Output is: {LongestConsecutives.LongestConsec(input, 2)}");
 public class LongestConsecutives 
 {
     
-    public static string[] LongestConsec(string[] strarr, int k) 
-    { 
-        string[] concatStrings = new string[strarr.Length];
-
-        for (int i = 0; i < strarr.Length-1; i++)
-        {
-          concatStrings[0] = string.Join("", strarr[i] + strarr[i+1]);
-        }
-
-        foreach (var element in concatStrings)
-        {
-          
-        }
-
-        return concatStrings;
+    public static string LongestConsec(string[] strarr, int k) 
+{ 
+    if (k <= 0 || k > strarr.Length)
+    {
+        return "";
     }
+
+    string[] concatStrings = new string[strarr.Length - k + 1];
+
+    for (int i = 0; i <= strarr.Length - k; i++)
+    {
+        string concatenated = "";
+        for (int j = 0; j < k; j++)
+        {
+            concatenated += strarr[i + j];
+        }
+        concatStrings[i] = concatenated;
+    }
+
+    int returnIndex = 0;
+
+    for (int i = 1; i < concatStrings.Length; i++)
+    {
+        if (concatStrings[i].Length > concatStrings[returnIndex].Length)
+        {
+            returnIndex = i;
+        }
+    }
+
+    return concatStrings[returnIndex];
+}
 }
